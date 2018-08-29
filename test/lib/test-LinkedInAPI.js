a1c25fdeae2c416ac4fb42d0b8a31f9e0a4e88d0
Alexander Grigoryan <agrigoryan1982@gmail.com>
2018-08-29 01:09:15 -0400

    added getAuthorizationUrl()

--- test/lib/test-LinkedInAPI.js
+++ test/lib/test-LinkedInAPI.js
@@ -4,9 +4,10 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const LinkedInAPI = require('../../lib/LinkedInAPI');
+const linkedInAPI = new LinkedInAPI('client_id', 'client_secret', 'redirect_uri');
 
 describe('test /lib/LinkedInAPI.js', () => {
-  const linkedInAPI = new LinkedInAPI();
+  beforeEach(() => {});
 
   it('should call getCurrentMemberProfile() correctly', (done) => {
     linkedInAPI.getCurrentMemberProfile()
@@ -19,5 +20,10 @@ describe('test /lib/LinkedInAPI.js', () => {
         console.log(err);
         done(err)
       });
+  });
+
+  it('should call getAuthorizationUrl() correctly', () => {
+    const url = linkedInAPI.getAuthorizationUrl(['r_basicprofile', 'r_basicprofile_2'], 'state');
+    expect(typeof url === 'string').to.be.true;
   });
 });

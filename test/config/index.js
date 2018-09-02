b45afb66b1d8cb12242556a21aa0aea2463615ec
agrig <agrigoryan1982@gmail.com>
2018-09-02 11:56:19 -0400

    added new functions, p.2

---
+++ test/config/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const config = {
+  // Test Organizations
+  // https://developer.linkedin.com/docs/guide/v2/organizations/development-testing
+
+  // DevTestCo
+  DevTestCo: {
+    name: 'DevTestCo',
+    url: 'https://www.linkedin.com/company/devtestco',
+    organization_urn: 'urn:li:organization:2414183',
+    id: '2414183'
+  },
+
+  // Test University
+  testUniversity: {
+    name: 'Test University',
+    url: 'http://www.linkedin.com/school/6177438',
+    organization_urn: 'urn:li:organization:6177438',
+    id: '6177438'
+  },
+};
+
+module.exports = config;
